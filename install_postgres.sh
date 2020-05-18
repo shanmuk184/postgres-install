5800e0ec7ba035cb2d9602418c378710c662413c
Shanmuk <shanmuksastry@gmail.com>
2020-05-19 02:18:13 +0530

    Update install_postgres.sh

--- install_postgres.sh
+++ install_postgres.sh
@@ -1,6 +1,7 @@
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
 sudo apt update
-sudo apt-get install postgresql
+sudo apt-get install postgresql-12
 sudo -i -u postgres psql -c "ALTER USER postgres with password 'new_password';"
 sudo -i -u postgres psql -c "CREATE USER Ubuntu with password 'password';"
 sudo -i -u postgres psql -c "CREATE DATABASE Test;"
